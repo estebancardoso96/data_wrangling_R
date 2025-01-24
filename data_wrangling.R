f18193c9a7dad543c26f731c438d51a93a464c01
Esteban Cardoso <ecardoso@ine.gub.uy>
2025-01-24 16:27:08 -0300

    segundo commit

--- data_wrangling.R
+++ data_wrangling.R
@@ -1,6 +1,10 @@
 library(tidyverse)
 library(dplyr)
+library(stringi) # librería que nos da la función str_squish
+
+registro <- data.frame('Nombre'=c('Jorge', 'Fiorella', 'Hector ', 'Gabriela', 'Jorge', 'Jorge'),
+                          'id'=c(1, 2, 3, 4, 1, 1),
+                          'fecha'=c(202401, 202401, 202401, 202401, 202401, 202402))
 
 
-
-
+registro
