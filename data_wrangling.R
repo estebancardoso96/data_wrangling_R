e79daa221571a678660c4fac524bbece585bd701
Esteban Cardoso <ecardoso@ine.gub.uy>
2025-01-24 16:52:48 -0300

    cuarto commir

--- data_wrangling.R
+++ data_wrangling.R
@@ -6,10 +6,22 @@ registro <- data.frame('Nombre'=c('Jorge', 'Fiorella', 'Hector ', 'Gabriela', 'J
                           'id'=c(1, 2, 3, 4, 1, 1),
                           'fecha'=c(202401, 202401, 202401, 202401, 202401, 202402))
 
-
 # Elimino espacios a los costados
 registro$Nombre <- str_squish(registro$Nombre)
+
+# Ver los duplicados (no incluye la primera aparición)
+registro[duplicated(registro), ]
 
 # Elimino las filas enteramente duplicadas
 registro <- registro %>% distinct()
 
+# Ver que tomo la primera aparición de id
+
+registro %>% distinct(id, .keep_all = TRUE)
+
+# Ver que tomo la primera aparición de id y fecha (como la fecha es distinta no elimina la última fila)
+
+registro %>% distinct(id, fecha,.keep_all = TRUE)
+
+
+registro <- registro %>% distinct(id, .keep_all = TRUE)
